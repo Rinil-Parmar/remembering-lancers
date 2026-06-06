b7b0d4268fc584beff981363e48304040513bb3f
Rinil-Parmar <parmarrinila2002@gmail.com>
2026-06-06 05:00:19 -0400

    refactor: move database models into application package

--- models.py
+++ models.py
@@ -1,50 +1,9 @@
-# models.py
-from flask_sqlalchemy import SQLAlchemy
+"""Backward-compatible model imports.
 
-db = SQLAlchemy()
+New code should import models from remembering_lancers.models.
+"""
 
-class Obituary(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    tags = db.Column(db.String(50), default='updated')# New field: Tags (comma-separated string for now)
-    name = db.Column(db.String(255))
-    first_name = db.Column(db.String(255))
-    last_name = db.Column(db.String(255))
-    birth_date = db.Column(db.String(50), nullable=True)
-    death_date = db.Column(db.String(50), nullable=True)
-    city = db.Column(db.String(255), nullable=True)
-    province = db.Column(db.String(255), nullable=True)
-    publication_date = db.Column(db.DateTime(timezone=True)) 
-    obituary_url = db.Column(db.String(255), unique=True, nullable=False)
-    family_information = db.Column(db.Text, nullable=True)
-    donation_information = db.Column(db.Text, nullable=True)
-    is_alumni = db.Column(db.Boolean, default=False)
-    funeral_home = db.Column(db.String(255), nullable=True) # New field: Funeral Home
-    latitude = db.Column(db.Float, nullable=True)
-    longitude = db.Column(db.Float, nullable=True)
+from remembering_lancers.extensions import db
+from remembering_lancers.models import DistinctObituary, Obituary
 
-    def __repr__(self):
-        return f"<Obituary {self.name} - {self.city}, {self.province}>"
-
-class DistinctObituary(db.Model): # New model for dist_obituary
-    __tablename__ = 'dist_obituary' # Specify the table name if it's different from class name
-
-    id = db.Column(db.Integer, primary_key=True)
-    tags = db.Column(db.String(50), default='updated') # New field: Tags (comma-separated string for now)
-    name = db.Column(db.String(255))
-    first_name = db.Column(db.String(255))
-    last_name = db.Column(db.String(255))
-    birth_date = db.Column(db.String(50), nullable=True)
-    death_date = db.Column(db.String(50), nullable=True)
-    city = db.Column(db.String(255), nullable=True)
-    province = db.Column(db.String(255), nullable=True)
-    publication_date = db.Column(db.DateTime(timezone=True)) 
-    obituary_url = db.Column(db.String(255), unique=True, nullable=False)
-    family_information = db.Column(db.Text, nullable=True)
-    donation_information = db.Column(db.Text, nullable=True)
-    is_alumni = db.Column(db.Boolean, default=False)
-    funeral_home = db.Column(db.String(255), nullable=True) # New field: Funeral Home
-    latitude = db.Column(db.Float, nullable=True)
-    longitude = db.Column(db.Float, nullable=True)
-
-    def __repr__(self):
-        return f"<DistinctObituary {self.name} - {self.city}, {self.province}>"
+__all__ = ["db", "Obituary", "DistinctObituary"]
---
+++ remembering_lancers/models.py
@@ -0,0 +1,49 @@
+from .extensions import db
+
+
+class Obituary(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    tags = db.Column(db.String(50), default="updated")
+    name = db.Column(db.String(255))
+    first_name = db.Column(db.String(255))
+    last_name = db.Column(db.String(255))
+    birth_date = db.Column(db.String(50), nullable=True)
+    death_date = db.Column(db.String(50), nullable=True)
+    city = db.Column(db.String(255), nullable=True)
+    province = db.Column(db.String(255), nullable=True)
+    publication_date = db.Column(db.DateTime(timezone=True))
+    obituary_url = db.Column(db.String(255), unique=True, nullable=False)
+    family_information = db.Column(db.Text, nullable=True)
+    donation_information = db.Column(db.Text, nullable=True)
+    is_alumni = db.Column(db.Boolean, default=False)
+    funeral_home = db.Column(db.String(255), nullable=True)
+    latitude = db.Column(db.Float, nullable=True)
+    longitude = db.Column(db.Float, nullable=True)
+
+    def __repr__(self):
+        return f"<Obituary {self.name} - {self.city}, {self.province}>"
+
+
+class DistinctObituary(db.Model):
+    __tablename__ = "dist_obituary"
+
+    id = db.Column(db.Integer, primary_key=True)
+    tags = db.Column(db.String(50), default="updated")
+    name = db.Column(db.String(255))
+    first_name = db.Column(db.String(255))
+    last_name = db.Column(db.String(255))
+    birth_date = db.Column(db.String(50), nullable=True)
+    death_date = db.Column(db.String(50), nullable=True)
+    city = db.Column(db.String(255), nullable=True)
+    province = db.Column(db.String(255), nullable=True)
+    publication_date = db.Column(db.DateTime(timezone=True))
+    obituary_url = db.Column(db.String(255), unique=True, nullable=False)
+    family_information = db.Column(db.Text, nullable=True)
+    donation_information = db.Column(db.Text, nullable=True)
+    is_alumni = db.Column(db.Boolean, default=False)
+    funeral_home = db.Column(db.String(255), nullable=True)
+    latitude = db.Column(db.Float, nullable=True)
+    longitude = db.Column(db.Float, nullable=True)
+
+    def __repr__(self):
+        return f"<DistinctObituary {self.name} - {self.city}, {self.province}>"

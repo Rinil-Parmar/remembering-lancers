fb3bc1aae1caf9562710df4cf764ec9ba2cf9cf1
Rinil-Parmar <parmarrinila2002@gmail.com>
2026-06-06 04:52:36 -0400

    refactor: add centralized configuration and extensions

---
+++ remembering_lancers/config.py
@@ -0,0 +1,38 @@
+import os
+
+
+class BaseConfig:
+    SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-change-me")
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL",
+        "postgresql://postgres:admin@localhost:5432/remembering_lancers_dev",
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        "pool_pre_ping": True,
+    }
+
+    SCRAPER_MAX_PAGES = int(os.environ.get("SCRAPER_MAX_PAGES", "1"))
+
+
+class DevelopmentConfig(BaseConfig):
+    DEBUG = True
+
+
+class ProductionConfig(BaseConfig):
+    DEBUG = False
+
+
+class TestingConfig(BaseConfig):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "TEST_DATABASE_URL",
+        "sqlite:///:memory:",
+    )
+
+
+CONFIGS = {
+    "development": DevelopmentConfig,
+    "production": ProductionConfig,
+    "testing": TestingConfig,
+}
